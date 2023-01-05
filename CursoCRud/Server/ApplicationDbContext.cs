ab724d552f5998eee49f82bcf0c7244a87d11890
yeferson.valencia <alejandro.yandd@gmail.com>
2023-01-05 17:36:04 -0500

    se crea el models con clase Persona, ademas se ajusta la conexion bd

---
+++ CursoCRud/Server/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CursoCRud.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoCRud.Server
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Persona> personas { get; set; }
+    }
+}
